119e09b7b4f3401d80503ebb6cf1a903cca458c4
MrLily <dinukaw.19@cse.mrt.ac.lk>
2022-05-23 02:01:07 +0530

    navbar updated

--- views/activationFailed.php
+++ views/activationFailed.php
@@ -13,16 +13,34 @@
 </head>
     
 <body>
-    <!-- Navigation Bar -->
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- start navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="box-shadow: 13px 13px 20px #cbced1;">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#" style="font-weight: bold; font-size: x-large; color: #174966; font-family: 'Roboto Slab', serif;">
-                <img src="Images/logo.png" alt="Avatar Logo" style="width:35px;" class="rounded-pill"> Name
+            <!-- navbar brand -->
+            <a class="navbar-brand" href="#" style="padding: 2px 2px; font-weight: bold; font-size: x-large; color: #174966; font-family: 'Roboto Slab', serif;">
+                <img src="Images/logo.png" alt="Avatar Logo" style="width:35px; object-fit: cover; margin: 1px; border-radius: 50%; box-shadow: 0px 0px 3px #5f5f5f, 0px 0px 0px 5px #ecf0f3, 8px 8px 15px #a7aaa7, -8px -8px 15px #fff;" class="rounded-pill"> &nbsp;&nbsp;Multi-Grammar
             </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <!-- button login -->
+                    <li class="nav-item">
+                        <a class="btn btn-blue" aria-current="page" href="login.php">Login</a>
+                    </li>
+                    &nbsp;
+                    <!-- button signup -->
+                    <li class="nav-item">
+                        <a class="btn btn-blue" aria-current="page" href="signup.php">Signup</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </nav> 
-
+    </nav>
+    <!-- end navbar -->
+    <br>
+    <!-- start page content -->
     <div class="wrapper">
         <div class="logo">
                 <img src="Images/no.jpg" alt="">
@@ -33,7 +51,7 @@
         <div class="text-center fs-6">
             Account activation failed!
             please sign up again.
-            <a href="signup.php">Sign Up</a>
+            <a href="signup.php" style="font-size: medium">Sign Up</a>
         </div>
     </div>
 </body>
