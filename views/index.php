8dde51d6311fb36c0f95d335465a06673452ff18
MrLily <dinukaw.19@cse.mrt.ac.lk>
2022-05-24 23:25:06 +0530

    Ïcon button

--- views/index.php
+++ views/index.php
@@ -11,6 +11,7 @@
     <link rel="shortcut icon" href="Images/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="CSS/index.css">
@@ -66,7 +67,8 @@
                         <option value="es-ES">Spanish</option>
                     </select>
                     <button class="btn btn-blue" id="sendbtn">Submit</button>
-                    <button class="btn btn-blue" id="speakbtn">Speak</button>
+                    <button class="btn btn-speak" id="speakbtn"><span class="material-symbols-outlined">volume_up</span></button>
+                    <button class="btn btn-copy" id="copybtn"><span class="material-symbols-outlined">content_copy</span></button>
                 </form>
                 <br> <!-- image upload area -->
                 <form method="post" enctype="multipart/form-data">
